83c41ff62598d8ad717ab07090efb9183ff6194b
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-10-26 14:08:53 +0700

    fix(app bar): make custom app bar losely coupled

--- lib/custom_appbar.dart
+++ lib/custom_appbar.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  final AppBar appBar;
-  final EdgeInsets padding;
-  const CustomAppBar({Key? key, required this.appBar, required this.padding}) : super(key: key);
+  final String title;
+  const CustomAppBar({Key? key, required this.title,})
+      : super(key: key);
 
   @override
   Size get preferredSize => const Size.fromHeight(56.0);
@@ -11,10 +11,27 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: padding,
+      padding: const EdgeInsets.symmetric(horizontal: 10),
       child: Center(
-        child: appBar,
-      ),
-    );
+        child: AppBar(
+            title: Text(
+              '${this.title}',
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15.0),
+                  bottomRight: Radius.circular(15.0),
+                ),
+                side: BorderSide(color: Colors.black, width: 2.0)),
+            centerTitle: true,
+            backgroundColor: Colors.white,
+          ),
+        ),
+      );
   }
 }
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -8,28 +8,7 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
-        appBar: AppBar(
-        // TODO: Apply horizontal margin to appbar
-        title: const Text(
-          'QUIZ APP',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 24,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(15.0),
-              bottomRight: Radius.circular(15.0),
-            ),
-            side: BorderSide(color: Colors.black, width: 2.0)),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-      ),
-        padding: EdgeInsets.symmetric(horizontal: 10),
-      ),
+      appBar: CustomAppBar(title: 'QUIZ APP'),
       body: MainContent(),
     );
   }
