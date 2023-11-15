1e15afc30d462ee06580af233d0b21bdbc43dcb5
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-11-15 21:22:56 +0700

    Make styles a module

--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -2,22 +2,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'custom_appbar.dart';
 import 'rounded_button.dart';
-
-
-const TextStyle quizHeaderStyle =
-    TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 32.0);
-
-const TextStyle quizQuestionStyle = TextStyle(
-  color: Colors.black,
-  fontWeight: FontWeight.w400,
-  fontSize: 16.0,
-);
-
-const TextStyle quizAnswerStyle = TextStyle(
-  color: Colors.black,
-  fontWeight: FontWeight.w700,
-  fontSize: 18.0,
-);
+import 'styles.dart';
 
 class QuizScreen extends StatelessWidget {
   const QuizScreen({Key? key}) : super(key: key);
---
+++ lib/styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const TextStyle quizHeaderStyle =
+    TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 32.0);
+
+const TextStyle quizQuestionStyle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 16.0,
+);
+
+const TextStyle quizAnswerStyle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w700,
+  fontSize: 18.0,
+);
