fb1352d453bfb34feb2054ba6e64573a853762ce
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-11-16 13:44:51 +0700

    Change cloud to sql and network to kubernetes

--- lib/model/questions.dart
+++ lib/model/questions.dart
@@ -11,8 +11,8 @@ class Question {
 var questions = [
   Question(name: 'Linux', imageAsset: 'assets/images/linux.jpg', questionNum: 10),
   Question(name: 'DevOps', imageAsset: 'assets/images/devops.webp', questionNum: 10),
-  Question(name: 'Network', imageAsset: 'assets/images/network.webp', questionNum: 10),
+  Question(name: 'Kubernetes', imageAsset: 'assets/images/kubernetes.png', questionNum: 10),
   Question(name: 'Programming', imageAsset: 'assets/images/programming.jpg', questionNum: 10),
   Question(name: 'Docker', imageAsset: 'assets/images/docker.webp', questionNum: 10),
-  Question(name: 'Cloud', imageAsset: 'assets/images/cloud.webp', questionNum: 10)
+  Question(name: 'SQL', imageAsset: 'assets/images/sql.jpg', questionNum: 10)
 ];
